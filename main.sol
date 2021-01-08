70c9bedb44cde43ffc30517d3bcd94853a85b390
YusukeOikawa <oikawayusuke12000@gmail.com>
2021-01-08 16:55:58 +0900

    今度こそ

--- main.sol
+++ main.sol
@@ -3,27 +3,6 @@
 //メッセージ
 
 contract Main{
-    // 貸し借りの履歴
-    // struct Date{
-    //     uint256 Year;
-    //     uint256 Month;
-    //     uint256 Day;
-    // }
-    // struct Book{//(本に対しての履歴がいるかも？)(著者)
-    //     address User;//借りている人
-    //     uint num;//固有番号
-    //     string name;//本の名前
-    //     uint256 status;//状態(0 : 貸し出し中, 1 : 在庫あり)
-    //     uint256 left;//同じ名前の本の在庫冊数
-    //     uint256 reserved_num; //予約人数
-    //     uint pages;
-    //     uint256 borrow_date;//貸出日
-    //     uint256 return_date;//返却日
-    //     uint256[] friends;//同じ名前の本の固有番号
-    //     Date Bdate;
-    //     Date Rdate;
-    // }
-
     struct Review {
         //address User;
         string comment;
@@ -42,17 +21,6 @@ contract Main{
     uint256 subjectNum = 0;
 
     Subject public temp;
-
-//     Book[] booklist;//index:固有番号
-//     Student[] studentlist;
-//     mapping(address => uint) public students; // 利用者のアドレス -> studentlistのインデックス
-//     int256 P = 100;//ポイント初期値
-//     address lib = address(0xfbE00a15070cdF61B86098e651f053655292b424);
-
-//     function random(uint256 num, uint seed) private view returns (uint256) {
-//         return uint256(keccak256(abi.encodePacked(block.timestamp, seed)))%(num-1) + 1;
-//    }
-
 
 
     function _review(string memory _id, string memory comment) public {
@@ -76,10 +44,7 @@ contract Main{
 
     function getReviews(string memory _id) public view returns (string memory) {
         if (subject[_id] != 0) {
-            string memory money;
-            string memory akane;
-            abi.encodePacked(money," ", akane);
-            // return subjects[subject[_id]].reviews;
+            return subjects[subject[_id]].reviews;
         } else {
             return temp.reviews;
         }
